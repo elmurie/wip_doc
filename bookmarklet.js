2052ebaf0612717fe53535abc04eafb91bdcf6c6
elmurie <elmurie@gmail.com>
2022-07-31 15:52:57 +0200

    -changed name to index -added reference bookmarklet file

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+(function () {
+    const headTag = document.querySelector('head');
+    const styleTag = document.querySelector('head > style');
+    styleTag.setAttribute("media", "screen, print");
+    headTag
+})();
